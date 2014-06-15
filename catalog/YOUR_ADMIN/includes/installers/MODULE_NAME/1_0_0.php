7d161e8d4a7297e5588612c62be694e2fabd3061
Larry Bislew <bislewl@gmail.com>
2014-06-14 22:46:37 -0500

    modifed so it would be easier to code in the future

--- catalog/YOUR_ADMIN/includes/installers/MODULE_NAME/1_0_0.php
+++ catalog/YOUR_ADMIN/includes/installers/MODULE_NAME/1_0_0.php
@@ -1,33 +1,14 @@
 <?php
-
-
-$configuration = $db->Execute("SELECT configuration_group_id FROM " . TABLE_CONFIGURATION_GROUP . " WHERE configuration_group_title = 'MODULE_GROUP_TITLE' ORDER BY configuration_group_id ASC;");
-if ($configuration->RecordCount() > 0) {
-  while (!$configuration->EOF) {
-    $db->Execute("DELETE FROM " . TABLE_CONFIGURATION . " WHERE configuration_group_id = " . $configuration->fields['configuration_group_id'] . ";");
-    $db->Execute("DELETE FROM " . TABLE_CONFIGURATION_GROUP . " WHERE configuration_group_id = " . $configuration->fields['configuration_group_id'] . ";");
-    $configuration->MoveNext();
-  }
-}
-
-$db->Execute("INSERT INTO " . TABLE_CONFIGURATION_GROUP . " (configuration_group_title, configuration_group_description, sort_order, visible) VALUES ('MODULE_NAME', 'Set MODULE Options', '1', '1');");
-$configuration_group_id = $db->Insert_ID();
-
-$db->Execute("UPDATE " . TABLE_CONFIGURATION_GROUP . " SET sort_order = " . $configuration_group_id . " WHERE configuration_group_id = " . $configuration_group_id . ";");
-
-$db->Execute("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES
-            ('Version', 'MODULE_VERSION', '1.0.0', 'Version installed:', " . $configuration_group_id . ", 0, NOW(), NOW(), NULL, NULL);"); 
+// use $configuration_group_id where needed
 
 // For Admin Pages
-
+/*
 $zc150 = (PROJECT_VERSION_MAJOR > 1 || (PROJECT_VERSION_MAJOR == 1 && substr(PROJECT_VERSION_MINOR, 0, 3) >= 5));
 if ($zc150) { // continue Zen Cart 1.5.0
   // delete configuration menu
-  $db->Execute("DELETE FROM admin_pages WHERE page_key = 'configMODULE' LIMIT 1;");
+  $db->Execute("DELETE FROM ".TABLE_ADMIN_PAGES." WHERE page_key = 'configMODULE' LIMIT 1;");
   // add configuration menu
   if (!zen_page_key_exists('configMODULE')) {
-    $configuration = $db->Execute("SELECT configuration_group_id FROM " . TABLE_CONFIGURATION . " WHERE configuration_key = 'MODULE_VERSION' LIMIT 1;");
-    $configuration_group_id = $configuration->fields['configuration_group_id'];
     if ((int)$configuration_group_id > 0) {
       zen_register_admin_page('configMODULE',
                               'BOX_MODULE', 
@@ -41,3 +22,5 @@ if ($zc150) { // continue Zen Cart 1.5.0
     }
   }
 }
+ * 
+ */
