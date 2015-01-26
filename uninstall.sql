b7c690332b9834bb1ac70af9387a15a1bd9d1f38
Larry Bislew <bislewl@gmail.com>
2015-01-26 07:20:35 -0600

    modified for easy use

--- uninstall.sql
+++ uninstall.sql
@@ -1,9 +1,8 @@
 SET @configuration_group_id=0;
 SELECT @configuration_group_id:=configuration_group_id
 FROM configuration
-WHERE configuration_key= 'BACK_IN_STOCK_VERSION'
+WHERE configuration_key= 'MODULE_VERSION'
 LIMIT 1;
 DELETE FROM configuration WHERE configuration_group_id = @configuration_group_id;
 DELETE FROM configuration_group WHERE configuration_group_id = @configuration_group_id;
-DELETE FROM admin_pages WHERE page_key = 'configPhysAttr';
-DELETE FROM admin_pages WHERE page_key = 'catalogPhysAttr';
+DELETE FROM admin_pages WHERE page_key = 'configModulePAge';
