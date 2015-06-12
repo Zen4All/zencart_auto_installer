711cd51e7b2f8de2f7d5a11b6679f7998d0237c7
Larry Bislew <bislewl@gmail.com>
2015-06-11 22:33:00 -0500

    correct missing/incorrect lic's

--- catalog/YOUR_ADMIN/includes/init_includes/init_MODULE_NAME.php
+++ catalog/YOUR_ADMIN/includes/init_includes/init_MODULE_NAME.php
@@ -1,11 +1,14 @@
 <?php
-/**
- * @package functions
- * @copyright Copyright 2003-2014 Zen Cart Development Team
+/* 
+ * 
+ * @package zencart_auto_installer
+ * @copyright Copyright 2003-2015 ZenCart.Codes a Pro-Webs Company
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: $
+ * 
  */
+
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
